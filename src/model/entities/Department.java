955b64c361c823d65c01a276bc5b335b745d7777
marcosr1 <marcosrichelly43@gmail.com>
2025-04-26 03:30:40 -0300

    Adding database access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -2,7 +2,7 @@ package model.entities;
 
 import java.io.Serializable;
 
-public class Department  implements Serializable {
+public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -62,5 +62,4 @@ public class Department  implements Serializable {
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-
 }
