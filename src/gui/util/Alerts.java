645986e2515a8a81b64a5671eb905b994039f5d4
marcosr1 <marcosrichelly43@gmail.com>
2025-04-30 23:41:55 -0300

    Remove department

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,7 +1,11 @@
 package gui.util;
 
+import java.lang.foreign.Linker.Option;
+import java.util.Optional;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 public class Alerts {
 	
@@ -12,5 +16,13 @@ public class Alerts {
 		alert.setContentText(content);
 		alert.show();
 	}
+	
+	public static Optional<ButtonType> showConfirmation(String title, String content) {
+		 Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		return alert.showAndWait();
+		}
 
 }
